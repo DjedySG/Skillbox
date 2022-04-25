33d51acbbf0df9716ee5ea03a7603225921e67f9
DjedySG <89001015+DjedySG@users.noreply.github.com>
2022-04-25 19:38:38 +0400

    Add files via upload

---
+++ Task_32_5_1.cpp
@@ -0,0 +1,27 @@
+﻿// Task_32_5_1.cpp: определяет точку входа для приложения.
+//
+
+#include "Task_32_5_1.h"
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+int main()
+{
+	json film_one = {
+		{"Name", "Kickboxer"},
+		{"Country", "USA"},
+		{"year", 1989},
+		{"Studio", "Kings Road Entertaiment"},
+		{"Scenario", "Glenn Brus"},
+		{"Directed by", "Mark Dissale"},
+		{"Produced by", "Mark Dissale" },
+		{"Main Actor", "Jan Clod Van Damm"},
+	};
+	std::ofstream file("record.json");
+	file << film_one;
+	file.close();
+	return 0;
+}
